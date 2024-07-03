de205feef54b1685328cdfcbb21eabdb0bf7da1c
hyunmang2 <wowow19811@gmail.com>
2024-07-03 17:29:28 +0900

    쿠키로 저장하는 부분 삭제

--- src/main/java/com/sparta/schedule/jwt/JwtUtil.java
+++ src/main/java/com/sparta/schedule/jwt/JwtUtil.java
@@ -4,7 +4,6 @@ import com.sparta.schedule.entity.UserRoleEnum;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -56,20 +53,9 @@ public class JwtUtil {
                         .signWith(key, signatureAlgorithm) // 암호화 알고리즘
                         .compact();
     }
-
-    // JWT Cookie 에 저장
-    public void addJwtToCookie(String token, HttpServletResponse res) {
-        try {
-            token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
-
-            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token); // Name-Value
-            cookie.setPath("/");
-
-            // Response 객체에 Cookie 추가
-            res.addCookie(cookie);
-        } catch (UnsupportedEncodingException e) {
-            logger.error(e.getMessage());
-        }
+    // header에 token 저장
+    public void addJwtToHeader(String tokenHeader, String token, HttpServletResponse res) {
+        res.addHeader(tokenHeader, token);
     }
 
     // JWT 토큰 substring
--- src/main/java/com/sparta/schedule/service/UserService.java
+++ src/main/java/com/sparta/schedule/service/UserService.java
@@ -1,29 +1,31 @@
 package com.sparta.schedule.service;
 
+import com.sparta.schedule.dto.LoginRequestDto;
+import com.sparta.schedule.dto.MessageResponseDto;
 import com.sparta.schedule.dto.SignupRequestDto;
 import com.sparta.schedule.entity.User;
 import com.sparta.schedule.entity.UserRoleEnum;
+import com.sparta.schedule.enums.UserStatusEnum;
+import com.sparta.schedule.jwt.JwtUtil;
 import com.sparta.schedule.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
+    private final JwtUtil jwtUtil;
     // ADMIN_TOKEN
     private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
-    public void signup(SignupRequestDto requestDto) {
+    public MessageResponseDto signup(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
         String password = passwordEncoder.encode(requestDto.getPassword());
 
@@ -41,9 +43,37 @@ public class UserService {
             }
             role = UserRoleEnum.ADMIN;
         }
-
         // 사용자 등록
-        User user = new User(username, password, role);
+        //Todo 인자로 requestDto.getNickname() 이렇게도 넣을 수 있다?
+        User user = new User(requestDto.getNickname(), username, password, role);
+
         userRepository.save(user);
+
+//        SignupResponseDto signupResponseDto = new SignupResponseDto(username, requestDto.getNickname());
+
+        return new MessageResponseDto("회원가입이 완료되었습니다.");
+    }
+
+    public MessageResponseDto login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        String nickname = loginRequestDto.getNickname();
+        String password = loginRequestDto.getPassword();
+        // 회원가입 된 유저인지 확인.
+        User user = findByNickname(nickname);
+        // 로그인 시 입력한 비밀번호와 입력한 비밀번호가 일치하는지 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        // Token 생성
+        String token = jwtUtil.createToken(user.getUsername(), UserRoleEnum.USER);
+        // Header에 Token 저장
+        jwtUtil.addJwtToHeader(JwtUtil.AUTHORIZATION_HEADER, token, response);
+
+        return new MessageResponseDto("로그인이 완료되었습니다.");
+    }
+
+    private User findByNickname(String nickname) {
+        return userRepository.findByNickname(nickname).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
     }
 }
